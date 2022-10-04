c8fe4472f0b0835da25d60e8b222c1cfcccc047c
Linard Hug <linard.hug@googlemail.com>
2022-10-04 18:05:45 +0200

    Rotate Camera & Improve Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
 {
     #region Inspector
 
+    [Header("Movement")]
+    
     [Min(0)]
     [Tooltip("The maximum speed of the player in uu/s.")]
     [SerializeField] private float movementSpeed = 5f;
@@ -13,15 +15,62 @@ public class PlayerController : MonoBehaviour
     [Tooltip("How fast the movement speed is in-/decreasing.")]
     [SerializeField] private float speedChangeRate = 10f;
 
+    [Min(0)]
+    [Tooltip("How fast the character rotates around it's y-axis.")]
+    [SerializeField] private float rotationSpeed = 10f;
+    
+    [Header("Camera")]
+
+    [Tooltip("The focus and rotation point of the camera.")]
+    [SerializeField] private Transform cameraTarget;
+
+    [Range(-89f, 0f)]
+    [Tooltip("The minimal vertical camera angle. Lower half of the horizon.")]
+    [SerializeField] private float verticalCameraRotationMin = -30f;
+
+    [Range(0f, 89f)]    
+    [Tooltip("The maximum vertical camera angle. Upper half of the horizon.")]
+    [SerializeField] private float verticalCameraRotationMax = 70f;
+
+    [Min(0)]
+    [Tooltip("Sensitivity of the horizontal camera rotation. deg/s for controller.")]
+    [SerializeField] private float cameraHorizontalSpeed = 200f;
+
+    [Min(0)]
+    [Tooltip("Sensitivity of the vertical camera rotation. deg/s for controller.")]
+    [SerializeField] private float cameraVerticalSpeed = 130f;
+
+    [Header("Mouse Settings")]
+
+    // TODO Put on PlayerPrefs and show in settings.
+    [Range(0f, 2f)]
+    [Tooltip("Additional mouse rotation speed multiplier.")]
+    [SerializeField] private float mouseCameraSensitivity = 1f;
+    
+    [Header("Controller Settings")]
+    
+    // TODO Put on PlayerPrefs and show in settings.
+    [Range(0f, 2f)]
+    [Tooltip("Additional controller rotation speed multiplier.")]
+    [SerializeField] private float controllerCameraSensitivity = 1f;
+
+    // TODO Put on PlayerPrefs and show in settings.
+    [Tooltip("Invert Y-axis for controller.")]
+    [SerializeField] private bool invertY = true;
+
     #endregion
 
     private CharacterController characterController;
     
     private GameInput input;
+    private InputAction lookAction;
     private InputAction moveAction;
 
+    private Vector2 lookInput;
     private Vector2 moveInput;
 
+    private Quaternion characterTargetRotation = Quaternion.identity;
+    private Vector2 cameraRotation;
     private Vector3 lastMovement;
 
     #region Unity Events Functions
@@ -31,7 +80,8 @@ public class PlayerController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         
         input = new GameInput();
-        // Cache the move action specifically for easier usage.
+        // Cache the look and move action specifically for easier usage.
+        lookAction = input.Player.Look;
         moveAction = input.Player.Move;
 
         // TODO Subscribe to input events.
@@ -44,10 +94,15 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        moveInput = moveAction.ReadValue<Vector2>();
-        
+        ReadInput();
+
         Rotate(moveInput);
         Move(moveInput);
+    }
+
+    private void LateUpdate()
+    {
+        RotateCamera(lookInput);
     }
 
     private void OnDisable()
@@ -62,6 +117,16 @@ public class PlayerController : MonoBehaviour
 
     #endregion
 
+    #region Input
+
+    private void ReadInput()
+    {
+        lookInput = lookAction.ReadValue<Vector2>();
+        moveInput = moveAction.ReadValue<Vector2>();
+    }
+
+    #endregion
+
     #region Movement
 
     private void Rotate(Vector2 moveInput)
@@ -69,7 +134,20 @@ public class PlayerController : MonoBehaviour
         if (moveInput != Vector2.zero)
         {
             Vector3 inputDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
-            transform.rotation = Quaternion.LookRotation(inputDirection);
+
+            Vector3 worldInputDirection = cameraTarget.TransformDirection(inputDirection);
+            worldInputDirection.y = 0f;
+            
+            characterTargetRotation = Quaternion.LookRotation(worldInputDirection);
+        }
+
+        if (Quaternion.Angle(transform.rotation, characterTargetRotation) > 0.1f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, characterTargetRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = characterTargetRotation;
         }
     }
 
@@ -81,6 +159,7 @@ public class PlayerController : MonoBehaviour
         currentVelocity.y = 0;
         float currentSpeed = currentVelocity.magnitude;
 
+        // Lets the character slowly approach the targetSpeed based on their currentSpeed.
         if (Mathf.Abs(currentSpeed - targetSpeed) > 0.01f)
         {
             currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, speedChangeRate * Time.deltaTime);
@@ -90,12 +169,84 @@ public class PlayerController : MonoBehaviour
             currentSpeed = targetSpeed;
         }
 
-        Vector3 movement = transform.forward * currentSpeed;
+        // Multiply the targetRotation Quaternion with Vector3.forward (not commutative!)
+        // to get a direction vector in the direction of the targetRotation.
+        // In a sense "vectorize the quaternion" (loosing one axis of data: the roll)
+        Vector3 targetDirection = characterTargetRotation * Vector3.forward;
+
+        Vector3 movement = targetDirection * currentSpeed;
 
         characterController.SimpleMove(movement);
 
         lastMovement = movement;
     }
 
+    #endregion
+
+    #region Camera
+
+    private void RotateCamera(Vector2 lookInput)
+    {
+        if (lookInput != Vector2.zero)
+        {
+            bool isMouseLook = IsMouseLook();
+
+            float deltaTimeMultiplier = isMouseLook ? 1f : Time.deltaTime;
+
+            float sensitivity = isMouseLook ? mouseCameraSensitivity : controllerCameraSensitivity;
+
+            lookInput *= deltaTimeMultiplier * sensitivity;
+            
+            // Multiply the input with the vertical camera speed in deg/s.
+            // Vertical camera rotation around the X-axis of the player!
+            // Additionally multiply with -1 if we are using the controller AND we want to invert the Y input.
+            cameraRotation.x += lookInput.y * cameraVerticalSpeed * (!isMouseLook && invertY ? -1 : 1);
+            
+            // Multiply the input with the horizontal camera speed in deg/s.
+            // Horizontal camera rotation around the Y-axis of the player!
+            cameraRotation.y += lookInput.x * cameraHorizontalSpeed;
+
+            cameraRotation.x = NormalizeAngle(cameraRotation.x);
+            cameraRotation.y = NormalizeAngle(cameraRotation.y);
+
+            cameraRotation.x = Mathf.Clamp(cameraRotation.x, verticalCameraRotationMin, verticalCameraRotationMax);
+        }
+
+        // Important to always do even without input, so it is always steady and only move if we give input.
+        // This prevents it from rotation with it's parent Player object.
+        // Create a new Quaternion with (x,y,z) rotation (like in the inspector).
+        cameraTarget.rotation = Quaternion.Euler(cameraRotation.x, cameraRotation.y, 0f);
+    }
+
+    private float NormalizeAngle(float angle)
+    {
+        // Limits the angle to (-360, 360).
+        angle %= 360;
+
+        // Limits the angle to [0, 360).
+        if (angle < 0)
+        {
+            angle += 360;
+        }
+
+        // Remaps the angle from [0, 360) to [-180, 180).
+        if (angle > 180)
+        {
+            angle -= 360;
+        }
+
+        return angle;
+    }
+
+    private bool IsMouseLook()
+    {
+        if (lookAction.activeControl == null)
+        {
+            return true;
+        }
+
+        return lookAction.activeControl.name == "delta";
+    }
+    
     #endregion
 }
